148a7563f271cecc5e510a3920fd3cd066d70cea
Wai Ho Vong <waihovong@Wais-Air.lan>
2020-08-12 07:03:56 +0930

    added in images and appjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import  {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Headroom from 'react-headroom';
+
+import SpaceX from '../src/assets/images/SpaceX-Logo.svg';
+
+import Navbar from './components/navbar';
+import LandingPage from './components/landingpage';
+import LatestLaunch from './components/latestlaunch';
+import FalconHeavy from './components/vehicle_falconheavy';
+import Starship from './components/vehicle_starship';
+import PreviousLaunch from './components/previous';
+import Launch from './components/launch';
+import F9 from './components/vehicle_falcon9';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Headroom>
+					< Navbar />
+				</Headroom>
+				<Switch>
+					<Route path='/missions'>
+						< PreviousLaunch />
+					</Route>
+					<Route path='/rocket/falcon9'>
+						< F9 />
+					</Route>
+					<Route path='/rocket/falconheavy'>
+						< FalconHeavy />
+					</Route>
+					<Route path='/rocket/starship'>
+						< Starship />
+					</Route>
+					<Route path='/launch/:id' component={Launch}></Route>
+					<Route path='/'>
+						< LandingPage />
+						< LatestLaunch />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
