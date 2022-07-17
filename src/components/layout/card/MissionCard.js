e07d0df1c5dc1e9c73a31a4aa28de2956bcb9b13
Wai Ho Vong <waihovong@gmail.com>
2022-07-17 23:57:46 +0930

    Removed unused code

--- src/components/layout/card/MissionCard.js
+++ src/components/layout/card/MissionCard.js
@@ -1,61 +1,51 @@
 import React from "react";
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import backgroundImages from "../../../../src/assets/images/Wallpapers/dragon.jpg"
 
-function MissionCard({missionName, time, status, flightNumber, rocket, launchPad, flightPatch}) {
+function MissionCard({missionName, time, status, flightNumber, rocket, launchPad, missionPatch}) {
     return (
-        <div className="container w-fit flex flex-row mx-auto p-4 bg-sky-900 rounded-md mb-5">
+        <div className="container w-fit flex flex-row justify-end p-4 bg-[#1e293b] rounded-md mb-5">
 			<div className="flex flex-row pr-4">
-				<img className="object-cover h-28 w-42" src={backgroundImages} />
+				<img className="object-contain h-28 w-48" src={missionPatch} />
 			</div>
 			<div className="container flex flex-row justify-around">
 				<div className="w-64 flex flex-col uppercase">
 					<div>
-						<label className="font-semibold text-gray-200">Mission Name</label>
+						<label className="font-bold text-gray-300 text-sm">Mission Name</label>
 					</div>
 					<div>
-						{/* <span>{missionName}</span> */}
 						<span className="text-white text-lg font-semibold whitespace-normal ...">{missionName}</span>
 					</div>
 					<div>
-						<span className="font-semibold text-gray-200">Flight</span>
+						<span className="font-bold text-gray-300 text-sm">Flight</span>
 					</div>
 					<div>
-						{/* <span>{flightNumber}</span> */}
 						<span className="text-white text-lg font-semibold">{flightNumber}</span>
 					</div>
 				</div>
 				<div className="w-64 flexflex-col uppercase">
 					<div>
-						<span className="font-semibold text-gray-200">Time (UTC)</span>
+						<span className="font-bold text-gray-300 text-sm">Time (UTC)</span>
 					</div>
 					<div>
-						{/* <span>{missionName}</span> */}
 						<span className="text-white text-lg font-semibold">{time}</span>
 					</div>
 					<div>
-						<span className="font-semibold text-gray-200">Rocket</span>
+						<span className="font-bold text-gray-300 text-sm">Rocket</span>
 					</div>
 					<div>
-						{/* <span>{flightNumber}</span> */}
 						<span className="text-white text-lg font-semibold">{rocket}</span>
 					</div>
 				</div>
 				<div className="w-40 flex flex-col uppercase">
 					<div>
-						<span className="font-semibold text-gray-200">status</span>
+						<span className="font-bold text-gray-300 text-sm">status</span>
 					</div>
 					<div>
-						{/* <span>{missionName}</span> */}
 						<span>{status}</span>
 					</div>
 					<div>
-						<span className="font-semibold text-gray-200">launchpad</span>
+						<span className="font-bold text-gray-300 text-sm">launchpad</span>
 					</div>
 					<div>
-						{/* <span>{flightNumber}</span> */}
 						<span className="text-white text-lg font-semibold">{launchPad}</span>
 					</div>
 				</div>
