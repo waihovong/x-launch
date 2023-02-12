4f2c85efa9173c098aa453865db9b6878d72a533
Wai Ho Vong <waihovong@gmail.com>
2023-02-12 20:12:14 +1030

    Updates

--- src/api/apiconstant.js
+++
@@ -1,5 +0,0 @@
-const apiConstant = "https://api.spacexdata.com/v4";
-export default apiConstant;
-
-//this doesn't work export default
-//might just have to call constant on all pages and then use normally after 
